46ae62cb3b07046e9d49e2d72639c93a7112ce94
chhongzh <>
2023-07-11 23:00:57 +0800

    新的内建函数toint

--- stdlib/builtin.py
+++ stdlib/builtin.py
@@ -1,3 +1,6 @@
+from boat.lib_runner import error_message
+
+
 def _print(*args):
     # args[Any]
 
@@ -6,3 +9,9 @@ def _print(*args):
 
 def _input(*args):
     return input("".join(map(str, args)))
+
+
+def _toint(a: str):
+    if not a.isdigit():
+        error_message("Invalid number.")
+    return int(a)
