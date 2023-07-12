b1af90cbb816e372f588ce4edda68f26d38e017a
chhongzh <>
2023-07-12 21:09:46 +0800

    0.0a3版本

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ sys.argv = [__file__, "bdist_wheel"]  # Replace the args
 print("--- 开始构建! ---")
 setup(
     name="boat",
-    version="0.0a1",
+    version="0.0a3",
     packages=["boat"],
     license="MIT",
     author="chhongzh",
